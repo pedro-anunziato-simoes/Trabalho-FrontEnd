b5a05865f61d4778fb12e9d2f6cbe14eb1fa2616
Pedro Henrique da Silva Gomes Aunziato Simões <145767878+pedro-anunziato-simoes@users.noreply.github.com>
2024-04-14 09:48:05 -0300

    Subi os aquivos do site

---
+++ index.js
@@ -0,0 +1,93 @@
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", pronto())
+} else {
+    pronto()
+}
+
+function pronto() {
+    const botaoRemoverItem = document.getElementsByClassName("botao-remover-item")
+    for (var i = 0; i < botaoRemoverItem.length; i++) {
+        botaoRemoverItem[i].addEventListener("click", removerItem)
+    }
+
+    const quantidadeItens = document.getElementsByClassName("quantidade-carrinho")
+    for (var i = 0; i < quantidadeItens.length; i++) {
+        quantidadeItens[i].addEventListener("change", checarInputNulo)
+    }
+
+    const botaoAdicionarItemCarrinho = document.getElementsByClassName("botao-adicionar-produto")
+    for (var i = 0; i < botaoAdicionarItemCarrinho.length; i++) {
+        botaoAdicionarItemCarrinho[i].addEventListener("click", adicionarCarrinho)
+    }
+}
+
+
+
+function checarInputNulo(event){
+   if(event.target.value === "0"){
+    event.target.parentElement.parentElement.remove()
+   }
+
+   atualziarTabela()
+}
+
+function adicionarCarrinho(event) {
+    const botao = event.target
+    const informacoesProduto = botao.parentElement.parentElement
+    const imagemProduto = informacoesProduto.getElementsByClassName("imagem-produto")[0].src
+    const tituloProduto = informacoesProduto.getElementsByClassName("titulo-produto")[0].innerText
+    const precoProduto = informacoesProduto.getElementsByClassName("preco-produto")[0].innerText
+
+    const nomeProdutoCarrinho = document.getElementsByClassName("titulo-item")
+    for (var i = 0; i < nomeProdutoCarrinho.length; i++) {
+        if (nomeProdutoCarrinho[i].innerText == tituloProduto) {
+            nomeProdutoCarrinho[i].parentElement.parentElement.getElementsByClassName("quantidade-carrinho")[0].value++
+                return
+        }
+
+    }
+    let novoProdutoCarinho = document.createElement("tr")
+    novoProdutoCarinho.classList.add("item-carrinho")
+
+    novoProdutoCarinho.innerHTML =
+        `
+        <td class="identificacao-item"><img src="${imagemProduto}" alt="${tituloProduto}"><strong
+                    class="titulo-item">${tituloProduto}</strong></td>
+            <td>
+                <span class="preco-item">${precoProduto}</span>
+            </td>
+            <td><input type="number" value="1" class="quantidade-carrinho"><button
+                    class="botao-remover-item">Remover</button></td>
+    `
+   
+    const corpoTabela = document.querySelector(".carrinho tbody")
+    corpoTabela.append(novoProdutoCarinho)
+    atualziarTabela()
+    novoProdutoCarinho.getElementsByClassName("quantidade-carrinho")[0].addEventListener("change",checarInputNulo)
+    novoProdutoCarinho.getElementsByClassName("botao-remover-item")[0].addEventListener("click",removerItem,atualziarTabela)
+}
+
+
+function removerItem(event) {
+    event.target.parentElement.parentElement.remove()
+    atualziarTabela()
+}
+
+
+function atualziarTabela() {
+    let totalItens = 0
+    const itemCarrinho = document.getElementsByClassName("item-carrinho")
+    for (var i = 0; i < itemCarrinho.length; i++) {
+        const precoItem = itemCarrinho[i].getElementsByClassName("preco-item")[0].innerText.replace("R$", "").replace(",", ".")
+        const quantidadeItem = itemCarrinho[i].getElementsByClassName("quantidade-carrinho")[0].value
+        totalItens += precoItem * quantidadeItem
+    }
+    totalItens = totalItens.toFixed(2)
+    totalItens = totalItens.replace(".", ",")
+    document.querySelector(".total-itens-carrinho span").innerText = "R$" + totalItens
+
+}
+
+
+
+
